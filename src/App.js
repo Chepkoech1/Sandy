c5d019eac6693091347702b46e1d17a8a7579f94
Kiprotich <shadrackr057@gmail.com>
2023-01-26 12:21:12 +0300

    fixed errors

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+// import { Routes , Route} from 'react-router-dom'
+// import { Route, Routes } from "react-router-dom";
+import NavBar from './components/NavBar';
+import Home from './components/Home'
+import Shelf from "./components/Shelf"
+// import Library from './Library';
+import Search from "./components/Search"
+import Login from './components/Login';
+// import {useState} from 'react'
+
 
 function App() {
+  // const [showSignUp, setShowSignUp] = useState(false);
+  // const handleClick = () => {
+  //   setShowSignUp(!showSignUp);
+  // }
+  // const [username, setUsername] = useState("");
+  // const [password, setPassword] = useState("");
+  // const handleLogin = () => {
+  //   // check if the entered username and password match any saved details
+  //   if (username === "savedUsername" && password === "savedPassword") {
+  //     // allow the user to login
+  //   } else {
+  //     setShowSignUp(true);
+  //   }
+  // }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    
+    <>
+       {/* <NavBar />
+       <Home/>
+       <Search /> */}
+       <Login/>
+      
+      
+    </>
   );
 }
-
 export default App;
+
+
+
+
+
+
